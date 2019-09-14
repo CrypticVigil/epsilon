bc0109b2af117025728ac6424b6ad27c99ef664a
Chad Barrows <chad.m.barrows@gmail.com>
2019-09-14 13:46:12 -0400

    added gulp styles function and browserSync

--- functions.php
+++ functions.php
@@ -1,12 +1,14 @@
 <?php
 
-function epsilon_styles() {
+function epsilon_assets() {
 
-	wp_enqueue_style( 'external-css', get_template_directory_uri() . '/css/add-styles.css' );
+	wp_enqueue_style( 'epsilon-css', get_template_directory_uri() . '/css/add-styles.css' );
+
+	wp_enqueue_script( 'epsilon-scripts', get_template_directory_uri() . '/js/scripts.js' );
 
 }
 
-add_action( 'wp_enqueue_scripts', 'epsilon_styles' );
+add_action( 'wp_enqueue_scripts', 'epsilon_assets' );
 add_theme_support( 'title-tag' );
 
 ?>
--- gulpfile.js
+++ gulpfile.js
@@ -2,16 +2,25 @@ const gulp = require('gulp');
 const sass = require('gulp-sass');
 const prefix = require('gulp-autoprefixer');
 const clean = require('gulp-clean-css');
+const browserSync = require('browser-sync').create();
 
-gulp.task('process', () =>
+gulp.task('styles', () =>
   gulp
     .src('scss/*.scss')
     .pipe(sass())
     .pipe(prefix())
     .pipe(clean())
     .pipe(gulp.dest('css'))
+    .pipe(browserSync.stream())
 );
 
-gulp.task('watch', () => gulp.watch('scss/*.scss', gulp.series('process')));
+gulp.task('dev', done => {
+  browserSync.init({
+    proxy: 'http://localhost:8888/',
+  });
+  gulp.watch('scss/*.scss', gulp.series('styles'));
+  gulp.watch('./*.php').on('change', browserSync.reload);
+  done();
+});
 
-gulp.task('default', gulp.series('watch'));
+gulp.task('default', gulp.series('dev'));
