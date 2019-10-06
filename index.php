32371e5a2659102c3b4c86801f2234df6b1a2741
Chad Barrows <chad.m.barrows@gmail.com>
2019-10-06 14:10:16 -0400

    added text to jumbotron

--- index.php
+++ index.php
@@ -1,7 +1,12 @@
 <?php get_header(); ?>
 
 <div class="jumbotron">
-	<img src="<?php echo get_bloginfo( 'template_directory' ); ?>/img/jumbotron-flower-lg.jpg" alt="A flower on a fuzzy green background">
+	<div class="jumbotron-text">Embracing life as our beautiful, broken,<br class="mobile-hide"> holy, <br class="desktop-hide"> messy selves.</div>
+	<img 
+		srcset="<?php echo get_bloginfo( 'template_directory' ); ?>/img/jumbotron-flower-sm.jpg 800w,
+						<?php echo get_bloginfo( 'template_directory' ); ?>/img/jumbotron-flower-lg.jpg 2160w"
+		sizes= "100vw"
+		src="<?php echo get_bloginfo( 'template_directory' ); ?>/img/jumbotron-flower-lg.jpg" alt="A flower on a fuzzy green background">
 </div>
 
 <div class="container">
