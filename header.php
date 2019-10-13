54bc5bbc398053b2f6c8f743a6b60c5fc6fa419d
Chad Barrows <chad.m.barrows@gmail.com>
2019-10-13 14:32:54 -0400

    accessibility changes

--- header.php
+++ header.php
@@ -7,7 +7,7 @@
 	<meta name="viewport" content="width=device-width, initial-scale=1.0">
 
 	<!-- Google Fonts -->
-	<link href="https://fonts.googleapis.com/css?family=Caveat:400,700|Roboto+Slab:100,300,400&display=swap" rel="stylesheet">
+	<link href="https://fonts.googleapis.com/css?family=Caveat|Roboto+Slab:300,400&display=swap" rel="stylesheet"> 
 
 	<?php wp_head(); ?>
 </head>
@@ -29,7 +29,7 @@
 			) ); ?>
 		</div>
 
-		<a href="javascript:void(0);" class="mobile-menu__link" onclick="navMenu()"><svg height="32px" id="mobile-menu__hamburger" style="enable-background:new 0 0 32 32;" version="1.1" viewBox="0 0 32 32" width="32px" xml:space="preserve" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink"><path d="M4,10h24c1.104,0,2-0.896,2-2s-0.896-2-2-2H4C2.896,6,2,6.896,2,8S2.896,10,4,10z M28,14H4c-1.104,0-2,0.896-2,2  s0.896,2,2,2h24c1.104,0,2-0.896,2-2S29.104,14,28,14z M28,22H4c-1.104,0-2,0.896-2,2s0.896,2,2,2h24c1.104,0,2-0.896,2-2  S29.104,22,28,22z"/></svg></a>
+		<a href="javascript:void(0);" class="mobile-menu__link" aria-label="Navigation Menu" onclick="navMenu()"><svg height="32px" id="mobile-menu__hamburger" alt="Menu" style="enable-background:new 0 0 32 32;" version="1.1" viewBox="0 0 32 32" width="32px" xml:space="preserve" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink"><path d="M4,10h24c1.104,0,2-0.896,2-2s-0.896-2-2-2H4C2.896,6,2,6.896,2,8S2.896,10,4,10z M28,14H4c-1.104,0-2,0.896-2,2  s0.896,2,2,2h24c1.104,0,2-0.896,2-2S29.104,14,28,14z M28,22H4c-1.104,0-2,0.896-2,2s0.896,2,2,2h24c1.104,0,2-0.896,2-2  S29.104,22,28,22z"/></svg></a>
 
 	</div>
 
--- searchform.php
+++ searchform.php
@@ -1,4 +1,4 @@
 <form role="search" method="get" action="<?php echo esc_url( home_url( '/' ) ) ?>">
-	<input type="search" name="s" placeholder="Search" value="<?php echo esc_attr(get_search_query())?>" />
-	<button type='submit'><img src="<?php echo get_bloginfo( 'template_directory' ); ?>/img/search.svg" alt="Search"></button>
+	<input type="search" name="s" placeholder="Search" aria-label="search bar" value="<?php echo esc_attr(get_search_query())?>" />
+	<button type='submit' aria-label="search button"><img src="<?php echo get_bloginfo( 'template_directory' ); ?>/img/search.svg" alt="Search"></button>
 </form>
